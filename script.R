a010e81b8228b07e59f945d470dcbdd055fa4d9d
G. Vituri <56522687+vituri@users.noreply.github.com>
2023-08-31 12:34:19 -0300

    add github page

--- script.R
+++ script.R
@@ -1,13 +1,38 @@
-library(vituripackage)
-arquivos = list.files(path = "Julia.jl-master", pattern = ".md", full.names = TRUE)
+library(dplyr)
+# arquivos = list.files(path = "Julia.jl-master", pattern = ".md", full.names = TRUE)
+#
+# arquivo = arquivos[1]
+#
+# destino = "joined_text.md"
+# write("", file = destino, append = FALSE)
+#
+# for (arquivo in arquivos) {
+#   texto = readLines(arquivo)
+#   write(texto, file = destino, append = TRUE)
+# }
 
-arquivo = arquivos[1]
+# create the .qmd
+dir.create("docs")
+preamble = '
+---
+title: "Awesome Julia!"
+format:
+  html:
+    toc: true
+    toc-depth: 3
+---
 
-destino = "joined_text.md"
-write("", file = destino, append = FALSE)
 
-for (arquivo in arquivos) {
-  texto = readLines(arquivo)
-  write(texto, file = destino, append = TRUE)
-}
+'
 
+output_file = 'docs/index.qmd'
+
+write(x = preamble, file = output_file, append = FALSE)
+
+readme = readLines("README.md")
+
+write(x = readme, file = output_file, append = TRUE)
+
+# render
+setwd("docs")
+quarto::quarto_render(input = "index.qmd", output_format = "html", output_file = "index.html")
